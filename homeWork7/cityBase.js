623ed8516a0b9ae8cb96002e66d0a73c2bca6a5c
AntonZubritski <a.zubritski@yandex.ru>
2019-12-19 16:15:36 +0300

    homeWork7

---
+++ homeWork7/cityBase.js
@@ -0,0 +1,19 @@
+const cityBase = [
+
+    { "city" : "Minsk",
+      "utc" : 0,
+      "pic" : "pic/minsk.jpg"
+    },
+    { "city" : "Moscow",
+      "utc" : 0,
+      "pic" : "pic/moscow.jpg"
+    },
+    { "city" : "Kazahstan",
+      "utc" : 5,
+      "pic" : "pic/budapest.jpeg"
+    },
+    { "city" : "Georgia",
+      "utc" : 4,
+      "pic" : "pic/prague.jpg"
+    }
+  ];
