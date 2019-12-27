f8d3363912a9be3a362bc208a444c9445dc81934
AntonZubritski <a.zubritski@yandex.ru>
2019-12-27 20:21:03 +0300

    homeWork11

---
+++ homeWork11/src/base/cityBase.js
@@ -0,0 +1,79 @@
+export const cityBase = [
+    { "city" : "NewYear",
+      "flickrText" : "Новый год",
+      "id" : 625144,
+      "pic" : "url(img/newYear.jpg)"
+    },
+    { "city" : "Minsk",
+      "flickrText" : "Minsk City",
+      "id" : 625144,
+      "pic" : "url(img/minsk.jpg)"
+    },
+    { "city" : "Moscow",
+      "flickrText" : "Moscow City",
+      "id" : 524901,
+      "pic" : "url(img/moscow.jpg)"
+    },
+    { "city" : "Budapest",
+      "flickrText" : "Budapest City",
+      "id" : 3054643,
+      "pic" : "url(img/budapest.jpeg)"
+    },
+    { "city" : "Prague",
+      "flickrText" : "Praga",
+      "id" : 3067696,
+      "pic" : "url(img/prague.jpg)"
+    },
+    { "city" : "Vilnius",
+        "flickrText" : "Vilnius City",
+        "id" : 593116,
+        "pic" : "url(img/vilnius.jpg)"
+    },
+    { "city" : "Warsaw",
+        "flickrText" : "Warsaw City",
+        "id" : 756135,
+        "pic" : "url(img/warsaw.jpg)"
+    },
+    { "city" : "Krakow",
+        "flickrText" : "Krakow city",
+        "id" : 3094802,
+        "pic" : "url(img/krakow.jpg)"
+    },
+    { "city" : "Lviv",
+        "flickrText" : "Lviv City",
+        "id" : 702550,
+        "pic" : "url(img/lviv.jpg)"
+    },
+    { "city" : "Vienna",
+        "flickrText" : "Vienna City",
+        "id" : 2761369,
+        "pic" : "url(img/vienna.jpg)"
+    },
+  ];
+
+export const infoWeather = [
+    {   "text" : "Выпьем для Сугреву, какаошко!",
+        "grad" : "-15"
+    },
+    {   "text": "Может пересмотришь Гарри Поттера?",
+        "grad" : "-10"
+    },
+    {   "text": "Отличная погода для игры в снежки, только снега нет",
+        "grad" : "-5"
+    },
+    {   "text": "Можно простыть!",
+        "grad" : "0"
+    },
+    {   "text": "Свитер, одень свитер!",
+        "grad" : "5"
+    },
+    {   "text": "Почти как весной",
+        "grad" : "10"
+    },
+    {   "text": "Почти как летом",
+        "grad" : "15"
+    },
+    {   "text": "Лето - можно расслабиться",
+        "grad" : "20"
+    },
+];
