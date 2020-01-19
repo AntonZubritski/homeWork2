efa0e5918d7300946db5ce6da43425b7100830f4
AntonZubritski <a.zubritski@yandex.ru>
2020-01-19 20:27:31 +0300

    diplom

---
+++ diplom/script/sidebar.js
@@ -0,0 +1,12 @@
+let dataToggle = document.getElementById("sidebarToggle");
+function sidebar(){
+    let sidebar = document.getElementById("sidebar");
+
+    if (sidebar.className.includes("open-sidebar")){
+        sidebar.classList.remove("open-sidebar");
+    }else {
+        sidebar.classList.add("open-sidebar");
+    }
+}
+
+dataToggle.addEventListener('click', () => sidebar());
---
+++ diplom/script/slider.js
@@ -0,0 +1,47 @@
+// $(document).ready(function(){
+//     $('.slider').bxSlider();
+// });
+
+$('.slider').bxSlider({
+    auto: true,
+    pager: true,
+    // adaptiveHeight:true,
+});
+
+// $('.main .sliderSale').bxSlider({
+//     auto: true,
+//     pager: false,
+//     pagerEl: false,
+//     adaptiveHeight: true,
+//     autoHover: true,
+// });
+
+
+    $('.main_slider ul').bxSlider({
+        pager: false,
+        controls: false,
+        mode: 'fade',
+        auto: true,
+        pause: 2000,
+        adaptiveHeight: true,
+        touchEnabled: false
+    });
+
+
+$(function() {
+    let pull 		= $('#pull');
+    menu 		= $('nav ul');
+    menuHeight	= menu.height();
+
+    $(pull).on('click', function(e) {
+        e.preventDefault();
+        menu.slideToggle();
+    });
+});
+
+$(window).resize(function(){
+    let w = $(window).width();
+    if(w > 320 && menu.is(':hidden')) {
+        menu.removeAttr('style');
+    }
+});
